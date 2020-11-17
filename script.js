48f3270dcb2c4b2dc058277af4439169748239cd
Jaros262 <matysekjaros@gmail.com>
2020-11-18 00:36:26 +0100

    fungujici, ale nedokonale a nedodelane

--- script.js
+++ script.js
@@ -3,61 +3,34 @@ const vypocet2 = document.getElementById('vypocet2');
 const vypocet3 = document.getElementById('vypocet3');
 const vypocet4 = document.getElementById('vypocet4');
 const c1 = document.getElementById('c1');
-const vysledek = document.getElementById('vysledek');
 const povrch = document.getElementById('povrch');
 const objem = document.getElementById('objem');
 const telU = document.getElementById('telU');
 const stenU = document.getElementById('stenU');
-/*const vzorceDruhy = [
-    { vyber: 'povrch', povrch},
-    { vyber: 'objem', objem},
-    { vyber: 'telU', telU},
-    { vyber: 'stenU', stenU}
-]*/
-
+const zaokrouhleni = document.getElementById('zaokrouhleni');
 
 c1.addEventListener('input', function(){
     c1.innerText = c1;
 })
 
 
-/*vypocet.addEventListener('click', function(){
-    console.log('done');
-    vysledek.innerText = c1.value * c1.value * 6;
-    console.log(vypocet);
-})*/
-
-/*vypocet.addEventListener('click', function(){
-    console.log('button clicked');
-    vysledek.innerText = c1.value * c1.value * 6;
-    povrch.innerHTML = 'Povrch =' (vypocet);
-    console.log(vypocet);
-})*/
-
-/*povrch.addEventListener('click', function(){
-    console.log('choosed path');
-    vysledek.innerText = c1.value * c1.value * 6;
-    console.log(vysledek);
-})*/
-
 povrch.addEventListener('click', function(){
     console.log('choosed path');
-    return vypocet1;
+    vypocet1.innerText = c1.value * c1.value * 6;
 })
 
 objem.addEventListener('click', function(){
     console.log('choosed path 2');
-    return vypocet2;
+    vypocet2.innerText = c1.value * c1.value * c1.value;
 })
 
-vypocet1.addEventListener('click', function(){
-    console.log('button clicked');
-    vysledek.innerText = c1.value * c1.value * 6;
-    console.log(vysledek);
+telU.addEventListener('click', function(){
+    console.log('choosed path 3');
+    vypocet3.innerHTML = c1.value * Math.sqrt("3");
+    
 })
 
-vypocet2.addEventListener('click', function(){
-    console.log('button clicked');
-    vysledek.innerText = c1.value * c1.value * c1.value;
-    console.log(vysledek);
+stenU.addEventListener('click', function(){
+    console.log('choosed path 4');
+    vypocet4.innerText = c1.value * Math.sqrt("2");
 })
