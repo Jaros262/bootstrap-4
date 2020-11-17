863f975f90ffc0a348226f09cfd62a73a76b8481
Jaros262 <matysekjaros@gmail.com>
2020-11-17 23:34:09 +0100

    work in progress. k teto verzi se vratim, ale ted udelam nejakou lehci

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const vypocet = document.getElementById('vypocet');
+const vypocet1 = document.getElementById('vypocet1');
+const vypocet2 = document.getElementById('vypocet2');
+const vypocet3 = document.getElementById('vypocet3');
+const vypocet4 = document.getElementById('vypocet4');
 const c1 = document.getElementById('c1');
 const vysledek = document.getElementById('vysledek');
-const vzorceDruhy = [
-    { vyber: 'povrch',},
-    { vyber: 'objem',},
-    { vyber: 'telU',},
-    { vyber: 'stenU',}
-]
+const povrch = document.getElementById('povrch');
+const objem = document.getElementById('objem');
+const telU = document.getElementById('telU');
+const stenU = document.getElementById('stenU');
+/*const vzorceDruhy = [
+    { vyber: 'povrch', povrch},
+    { vyber: 'objem', objem},
+    { vyber: 'telU', telU},
+    { vyber: 'stenU', stenU}
+]*/
 
 
 c1.addEventListener('input', function(){
@@ -20,8 +27,37 @@ c1.addEventListener('input', function(){
     console.log(vypocet);
 })*/
 
-povrch.addEventListener('click', function(){
-    console.log('done');
+/*vypocet.addEventListener('click', function(){
+    console.log('button clicked');
     vysledek.innerText = c1.value * c1.value * 6;
+    povrch.innerHTML = 'Povrch =' (vypocet);
     console.log(vypocet);
+})*/
+
+/*povrch.addEventListener('click', function(){
+    console.log('choosed path');
+    vysledek.innerText = c1.value * c1.value * 6;
+    console.log(vysledek);
+})*/
+
+povrch.addEventListener('click', function(){
+    console.log('choosed path');
+    return vypocet1;
+})
+
+objem.addEventListener('click', function(){
+    console.log('choosed path 2');
+    return vypocet2;
+})
+
+vypocet1.addEventListener('click', function(){
+    console.log('button clicked');
+    vysledek.innerText = c1.value * c1.value * 6;
+    console.log(vysledek);
+})
+
+vypocet2.addEventListener('click', function(){
+    console.log('button clicked');
+    vysledek.innerText = c1.value * c1.value * c1.value;
+    console.log(vysledek);
 })
