141f1ae2d83521e53c4fbb1316c3b804ab7fcbb1
Jaros262 <matysekjaros@gmail.com>
2020-11-19 00:11:34 +0100

    predelany projekt

--- script.js
+++ script.js
@@ -1,36 +1,23 @@
-const vypocet1 = document.getElementById('vypocet1');
-const vypocet2 = document.getElementById('vypocet2');
-const vypocet3 = document.getElementById('vypocet3');
-const vypocet4 = document.getElementById('vypocet4');
 const c1 = document.getElementById('c1');
-const povrch = document.getElementById('povrch');
-const objem = document.getElementById('objem');
-const telU = document.getElementById('telU');
-const stenU = document.getElementById('stenU');
-const zaokrouhleni = document.getElementById('zaokrouhleni');
+const vysledek = document.getElementById('vysledek');
+const vyber = [
+    { option: 'S', value: function() { return 6 * Math.pow(c1.value,2)} },
+    { option: 'V', value: function() { return Math.pow(c1.value,3)} },
+    { option: 'u', value: function() { return c1.value * Math.sqrt('3')} },
+    { option: 'us', value: function() { return c1.value * Math.sqrt('2')} }
+]
+
 
 c1.addEventListener('input', function(){
     c1.innerText = c1;
 })
 
-
-povrch.addEventListener('click', function(){
-    console.log('choosed path');
-    vypocet1.innerText = c1.value * c1.value * 6;
-})
-
-objem.addEventListener('click', function(){
-    console.log('choosed path 2');
-    vypocet2.innerText = c1.value * c1.value * c1.value;
-})
-
-telU.addEventListener('click', function(){
-    console.log('choosed path 3');
-    vypocet3.innerHTML = c1.value * Math.sqrt("3");
-    
-})
-
-stenU.addEventListener('click', function(){
-    console.log('choosed path 4');
-    vypocet4.innerText = c1.value * Math.sqrt("2");
+vysledek.addEventListener('click', function(){
+    /* Ukázka, jak můžete ze seznamu s id "vyber" zjistit zrovna vybranou hodnotu */
+    console.log(document.getElementById('vyber').value);
+    /* Pomocí metody find můžete v poli objektů vyber najít objekt podle uživatelovy volby v seznamu
+    a využít výpočtové funkce value() k zobrazení výsledku */
+    let vypocet = vyber.find(function(obj) { return obj.option === document.getElementById('vyber').value});
+    console.log(vypocet.value());
+    document.getElementById('vypocet').innerHTML = `${document.getElementById('vyber').value} = ${vypocet.value().toFixed(2)}`;
 })
